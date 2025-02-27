ece112a10c5faf73cf9fbbdb84fd5e310a4b78a7
winston <andyredjohn@gmail.com>
2025-02-27 11:05:42 -0600

    Loaded product data from csv

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   validates :title, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :stock_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  belongs_to :category
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-require 'faker'
+# require 'faker'
 
-676.times do
-  Product.create(
-    title: Faker::Commerce.product_name,
-    description: Faker::Lorem.paragraph,
-    price: Faker::Commerce.price(range: 10.0..100.0),
-    stock_quantity: Faker::Number.between(from: 1, to: 100)
+# 676.times do
+#   Product.create(
+#     title: Faker::Commerce.product_name,
+#     description: Faker::Lorem.paragraph,
+#     price: Faker::Commerce.price(range: 10.0..100.0),
+#     stock_quantity: Faker::Number.between(from: 1, to: 100)
+#   )
+# end
+require 'csv'
+
+Product.destroy_all
+Category.destroy_all
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+products.each do |row|
+  category = Category.find_or_create_by(name: row['category_name'])
+
+  Product.create!(
+    title: row['title'],
+    description: row['description'],
+    price: row['price'],
+    stock_quantity: row['stock_quantity'],
+    category: category
   )
 end
