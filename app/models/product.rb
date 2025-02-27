46a4316de9820abaebfce2bacb5f2f5f895d13e1
winston <andyredjohn@gmail.com>
2025-02-27 09:23:21 -0600

    Added validations for Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :title, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :stock_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
